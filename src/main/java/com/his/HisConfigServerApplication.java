66e9bfc3fdf7444f674804e5c4b71094ccf26464
sandyachoppara <infotosandya@gmail.com>
2024-12-07 09:00:16 +0530

    Code Pushed

---
+++ src/main/java/com/his/HisConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class HisConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisConfigServerApplication.class, args);
+	}
+
+}
